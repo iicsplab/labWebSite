047c45588e50bf6746684c1f6aba0361dcf3b737
xzj <1353753283@qq.com>
2025-05-19 20:12:32 +0800

    update

--- src/const/api_key.ts
+++ src/const/api_key.ts
@@ -16,7 +16,7 @@ export const filename = 'seminarInfo.json';
 // 具有读写github组会信息权限的token(！已经过移位加密对大写字母加密，如需查看，需解密后查看:'@/utils/common/rot13')
 // 此处不直接放原始token的原因是：该项目的代码存在github仓库，此token又是githun的token，github会进行检测，不允许仓库中包含token
 // export const token = 'ghp_bTFB3xgxdyDehMyrabtaO8EpoD2cma4EMo1i';
-export const token = 'ghp_p7mxPZYazIGUuBLQBKpiaI2dMSL84R2nIGan';
+export const token = 'ghp_hxRpxLokHV1M0hl3gyuHnybtkhwNWP1dwLfk';
 // 该token具有读写xx账号xx仓库的权限
 // token配置方法，参考：
 // https://segmentfault.com/a/1190000015144126
